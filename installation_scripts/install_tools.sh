16c1ef78e0631c83248a292e1ba43ae456f29e56
tiborauer <tibor.auer@gmail.com>
2023-02-20 15:25:51 +0000

    FIX - circleci

--- installation_scripts/install_tools.sh
+++ installation_scripts/install_tools.sh
@@ -1,12 +1,12 @@
 # This is a script to download tools also used in the tests. It requires a set of variables (URL_<tool name with lower case>) describing the source.
-# See <aa repo>/.github/workflows/tools_urls.sh as examples.
+# See <repo>/.circleci/tools_urls.sh as examples.
 # The script also checks the installation by looking for the corresponding folders.
 #
 # source install_tools.sh <parameterset> <installation_directory>
 #
 # Input aguments
 #   - parameterset:             XML parameterset containing information of required tools
-#   - installation_directory:   This is where FaceMasking will be installed. When configuring aap.directory_conventions.FaceMaskingdir MUST point to '<installation_directory>/mask_face/nrg-improc'
+#   - installation_directory:   This is where FaceMasking will be installed. When configuring rap.directoryconventions.facemaskingdir MUST point to '<installation_directory>/mask_face/nrg-improc'
 
 PARAMXML=$1
 INSTDIR=$2
@@ -40,18 +40,18 @@ CWD=$PWD
 cd ${INSTDIR}
 
 # Toolboxes
-tbxCount=$(xmllint --xpath 'count(//aap/local/directory_conventions/toolbox)' $PARAMXML)
+tbxCount=$(xmllint --xpath 'count(//rap/local/directoryconventions/toolbox)' $PARAMXML)
 for indTbx in `seq $tbxCount`; do
-    tbxName=$(xmllint --xpath '//aap/local/directory_conventions/toolbox['$indTbx']/name/text()' $PARAMXML)
+    tbxName=$(xmllint --xpath '//rap/local/directoryconventions/toolbox['$indTbx']/name/text()' $PARAMXML)
     varURL=URL_${tbxName}
-    tbxDir=$(xmllint --xpath '//aap/local/directory_conventions/toolbox['$indTbx']/dir/text()' $PARAMXML)
+    tbxDir=$(xmllint --xpath '//rap/local/directoryconventions/toolbox['$indTbx']/dir/text()' $PARAMXML)
     install_tool $tbxName ${!varURL} $tbxDir
 done
 
 # Dedicated tools
-# - FaceMasking from WUSLT NRG
-if [[ $(xmllint --xpath 'count(//aap/local/directory_conventions/FaceMaskingdir)' $PARAMXML) > 0 ]]; then
-    install_tool FaceMasking $URL_facemasking $(xmllint --xpath '//aap/local/directory_conventions/FaceMaskingdir/text()' $PARAMXML)
-fi
+# # - FaceMasking from WUSLT NRG
+# if [[ $(xmllint --xpath 'count(//rap/local/directoryconventions/facemaskingdir)' $PARAMXML) > 0 ]]; then
+#    install_tool FaceMasking $URL_facemasking $(xmllint --xpath '//rap/local/directoryconventions/facemaskingdir/text()' $PARAMXML)
+# fi
 
 cd $CWD
