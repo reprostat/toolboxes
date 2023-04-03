529188ea9539b07fa06089d03f3154ad225540e0
tiborauer <tibor.auer@gmail.com>
2023-04-03 22:13:07 +0100

    FIX - install FSL >6.0.5.2

--- installation_scripts/install_fsl.sh
+++ installation_scripts/install_fsl.sh
@@ -5,31 +5,30 @@
 # Input aguments
 #   - installation_directory:   This is where freesurfer will be installed. When configuring aap.directory_conventions.fsldir MUST point to '<installation_directory>/fsl'
 #   - version:                  Version number of the desired installation according to semantic versioning (x.y.z)
-#   - os:                       ID of OS the downloaded version has been compiled for
 #   - standard_only:            Only fsl/data/standard is installed. The whole FSL is not installed.
 #   - config_file:              Full path to the FSL configuration script to be generated
 
 INSTDIR=$1
 VERSION=$2
-FSOS=$3
-STANDARDONLY=$4
-CONFIGFILE=$5
+STANDARDONLY=$3
+CONFIGFILE=$4
 
 function fslconfig_bash {
-    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:INSTDIR/fsl/fslpython/lib:INSTDIR/fsl/lib'
     echo 'export FSLDIR=INSTDIR/fsl'
-    echo 'source $FSLDIR/etc/fslconf/fsl.sh'
     echo 'export PATH=$FSLDIR/bin:$PATH'
+    echo 'source $FSLDIR/etc/fslconf/fsl.sh'
 }
 
 cd ${INSTDIR}
-echo "Installing fsl-${VERSION}-${FSOS} in ${INSTDIR}"
-wget -q http://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-${VERSION}-${FSOS}_64.tar.gz
+echo "Installing FSL ${VERSION} in ${INSTDIR}"
+
 if [[ "x${STANDARDONLY}x" == "x1x" ]]; then
-    tar xzf fsl-${VERSION}-${FSOS}_64.tar.gz fsl/data/standard
+    wget -q http://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-6.0.5.2-centos7_64.tar.gz
+    tar xzf fsl-6.0.5.2-centos7_64.tar.gz fsl/data/standard
+    rm fsl-6.0.5.2-centos7_64.tar.gz
 else
-    tar xzf fsl-${VERSION}-${FSOS}_64.tar.gz
-    ${INSTDIR}/fsl/etc/fslconf/post_install.sh -f ${INSTDIR}/fsl
+    wget -q https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/releases/fslinstaller.py
+    python fslinstaller.py -V $VERION -d ${INSTDIR}/fsl -s 1 -m 1
 
     # config script
     if [[ $(basename $(echo $SHELL)) == "bash" ]]; then
@@ -40,8 +39,9 @@ else
         echo "FSL has not been installed properly. Exiting..." >&2
         exit -1; 
     fi
+
+    rm ${INSTDIR}/fslinstaller.py
 fi
-rm fsl-${VERSION}-${FSOS}_64.tar.gz
 
 if [[ -d "${INSTDIR}/fsl" ]]; then
     echo "FSL has been installed." >&2
