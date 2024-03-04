310c6800d198a469ddd7232c8de441b82a6f8caa
tiborauer <tibor.auer@gmail.com>
2024-03-04 13:12:52 +0000

    FIX - connClass when deployed

--- connClass.m
+++ connClass.m
@@ -3,26 +3,26 @@ classdef connClass < toolboxClass
         hGUI = []% GUI handles
         CONN_x0
     end
-       
+
     methods
         function obj = connClass(path,varargin)
             defaultAddToPath = false;
-            
+
             argParse = inputParser;
-            argParse.addRequired('path',@ischar);
+            argParse.addRequired('path',@(x) ischar(x) || isstruct(x));
             argParse.addParameter('name','',@ischar);
             argParse.addParameter('doAddToPath',defaultAddToPath,@(x) islogical(x) || isnumeric(x));
             argParse.parse(path,varargin{:});
-            
+
             vars = {...
                 '{"name": "CONN_x", "attributes": ["global"]}'...
                 '{"name": "CONN_h", "attributes": ["global"]}'...
                 '{"name": "CONN_gui", "attributes": ["global"]}'...
                 };
-            
+
             obj = obj@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,vars);
         end
-        
+
         function load(obj)
             addpath(obj.toolPath);
             if obj.showGUI
@@ -31,10 +31,10 @@ classdef connClass < toolboxClass
             else
                 conn('init');
             end
-            
+
             global CONN_x
             if ~obj.showGUI, CONN_x.gui = 0; end
-            
+
             % clear
             CONN_x.Setup.rois.names = {};
             CONN_x.Setup.rois.files = {};
@@ -49,13 +49,13 @@ classdef connClass < toolboxClass
             CONN_x.Setup.conditions.names = {''};
             CONN_x.Analyses(1) = [];
             CONN_x.Results(1) = [];
-            
+
             obj.CONN_x0 = CONN_x;
-            
+
             load@toolboxClass(obj)
         end
-        
-        function reset(obj)            
+
+        function reset(obj)
             global CONN_x
             CONN_x = obj.CONN_x0;
         end
