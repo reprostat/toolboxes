6e2f17f311c00b53935cdf6f6327bb1c0ef0a92f
tiborauer <tibor.auer@gmail.com>
2025-02-26 17:10:37 +0100

    FIX - eeglab and fieldtrip integration

--- eeglabClass.m
+++ eeglabClass.m
@@ -2,33 +2,33 @@ classdef eeglabClass < toolboxClass
     properties
         requiredPlugins = {}
     end
-    
+
     properties (Access = private)
         plugins = []
     end
-    
+
     properties (Access = protected, Dependent)
         hGUI% GUI handles
     end
-    
+
     properties (Dependent)
         dipfitPath
     end
-    
+
     methods
-        function obj = eeglabClass(path,varargin)
+        function this = eeglabClass(path,varargin)
             defaultAddToPath = true;
             defaultRequiredPlugins = {};
             defaultKeepGUI = false;
-            
+
             argParse = inputParser;
-            argParse.addRequired('path',@ischar);
+            argParse.addRequired('path',@(x) ischar(x) || isstruct(x));
             argParse.addParameter('name','',@ischar);
             argParse.addParameter('doAddToPath',defaultAddToPath,@(x) islogical(x) || isnumeric(x));
             argParse.addParameter('requiredPlugins',defaultRequiredPlugins,@(x) iscellstr(x) || isstruct(x));
             argParse.addParameter('doKeepGUI',defaultKeepGUI,@(x) islogical(x) || isnumeric(x));
             argParse.parse(path,varargin{:});
-            
+
             vars = {...
                 '{"name": "ALLCOM"}'...
                 '{"name": "ALLEEG"}'...
@@ -40,31 +40,33 @@ classdef eeglabClass < toolboxClass
                 '{"name": "PLUGINLIST"}'...
                 '{"name": "STUDY"}'...
                 };
-            
-            obj = obj@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,vars);
-            
-            obj.requiredPlugins = argParse.Results.requiredPlugins;
+
+            this = this@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,vars);
+
+            this.requiredPlugins = argParse.Results.requiredPlugins;
+
+            this.updateAfterLoadedFromStruct();
         end
-        
-        function load(obj,keepWorkspace)
+
+        function load(this,keepWorkspace)
             % backward compatibility
-            if ~isstruct(obj.requiredPlugins)
-                obj.requiredPlugins = cell2struct([obj.requiredPlugins;repmat({[]},2,10)],{'name','version','doPostprocess'});
+            if ~isstruct(this.requiredPlugins)
+                this.requiredPlugins = cell2struct([this.requiredPlugins;repmat({[]},3,10)],{'name','version','doPostprocess'});
             end
-            
+
             if nargin < 2, keepWorkspace = false; end
-            addpath(obj.toolPath);
+            addpath(this.toolPath);
             is_new_plugin = false;
-            if ~obj.showGUI, eeglab('nogui');
+            if ~this.showGUI, eeglab('nogui');
             else, eeglab;
             end
-            obj.plugins = evalin('base','PLUGINLIST');
-            
+            this.plugins = evalin('base','PLUGINLIST');
+
             plPost = {};
-            pllist = plugin_getweb('', obj.plugins, 'newlist');
+            pllist = plugin_getweb('', this.plugins, 'newlist');
             for p = reshape(pllist,1,[])
-                if any(strcmp({obj.requiredPlugins.name}, p.name)) && ~p.installed
-                    plInf = obj.requiredPlugins(strcmp({obj.requiredPlugins.name},p.name));
+                if any(strcmp({this.requiredPlugins.name}, p.name)) && ~p.installed
+                    plInf = this.requiredPlugins(strcmp({this.requiredPlugins.name},p.name));
                     if isfield(plInf,'version') && ~isempty(plInf.version)
                         if isnumeric(plInf.version), plInf.version = num2str(plInf.version); end
                         p.version = plInf.version;
@@ -72,43 +74,43 @@ classdef eeglabClass < toolboxClass
                         p.size = 1; % force install
                     end
                     if ~isempty(plInf.doPostprocess) && plInf.doPostprocess, plPost(end+1) = {p.name}; end
-                    
+
                     plugin_install(p.zip, p.name, p.version, p.size);
-                    
+
                     is_new_plugin = true;
                 end
             end
-            if is_new_plugin, obj.load; end
+            if is_new_plugin, this.load; end
 
-            % postprocess 
+            % postprocess
             for p = reshape(plPost,1,[])
                 fprintf('info:post-process installtion of %s\n', p{1});
-                obj.(['postprocess_' p{1}])();
+                this.(['postprocess_' p{1}])();
             end
-            
-            load@toolboxClass(obj,keepWorkspace)
+
+            load@toolboxClass(this,keepWorkspace)
         end
-        
-        function val = get.hGUI(obj)
-            if ~obj.showGUI, val = [];
+
+        function val = get.hGUI(this)
+            if ~this.showGUI, val = [];
             else
                 val = findall(allchild(0),'Flat','Tag','EEGLAB');
             end
         end
-        
-        function val = get.dipfitPath(obj)
+
+        function val = get.dipfitPath(this)
             [~,~,pl] = plugin_status('dipfit');
-            val = fullfile(obj.toolPath,'plugins',pl.foldername);
+            val = fullfile(this.toolPath,'plugins',pl.foldername);
         end
     end
-    
+
     methods (Access = private)
-        function postprocess_AMICA(obj)
+        function postprocess_AMICA(this)
             [~,~,pl] = plugin_status('AMICA');
-            plPath = fullfile(obj.toolPath,'plugins',pl.foldername);
+            plPath = fullfile(this.toolPath,'plugins',pl.foldername);
             if isunix
-               fileattrib(fullfile(plPath,'amica15ex'),'+x'); 
-               fileattrib(fullfile(plPath,'amica15ub'),'+x'); 
+               fileattrib(fullfile(plPath,'amica15ex'),'+x');
+               fileattrib(fullfile(plPath,'amica15ub'),'+x');
                % shadow amica15ex with amica15ub (N.B.: revert if you work on the Expanse Supercomputer)
                movefile(fullfile(plPath,'amica15ex'),fullfile(plPath,'bcp_amica15ex'));
                system(sprintf('ln -s %s %s',fullfile(plPath,'amica15ub'),fullfile(plPath,'amica15ex')));
--- fieldtripClass.m
+++ fieldtripClass.m
@@ -2,22 +2,24 @@ classdef fieldtripClass < toolboxClass
     properties (Access = protected)
         hGUI = []% GUI handles
     end
-    
+
     methods
-        function obj = fieldtripClass(path,varargin)
+        function this = fieldtripClass(path,varargin)
             defaultAddToPath = false;
-            
+
             argParse = inputParser;
-            argParse.addRequired('path',@ischar);
+            argParse.addRequired('path',@(x) ischar(x) || isstruct(x));
             argParse.addParameter('name','',@ischar);
             argParse.addParameter('doAddToPath',defaultAddToPath,@(x) islogical(x) || isnumeric(x));
             argParse.parse(path,varargin{:});
-            
-            obj = obj@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,{});
+
+            this = this@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,{});
+
+            this.updateAfterLoadedFromStruct();
         end
-        
-        function load(obj)
-            addpath(obj.toolPath);
+
+        function load(this)
+            addpath(this.toolPath);
             ft_defaults
             spmVer = '';
             try spmVer = spm('ver'); catch, warning('SPM is not detected'); end
@@ -27,19 +29,19 @@ classdef fieldtripClass < toolboxClass
                 ft_default.showcallinfo = 'no';
                 if ~isempty(spmVer), ft_default.spmversion = lower(spmVer); end
             end
-            
-            load@toolboxClass(obj)
+
+            load@toolboxClass(this)
         end
-        
-        function close(obj)
+
+        function close(this)
             global ft_default
             ft_default = [];
             clear ft_default;
-            close@toolboxClass(obj)
-        end        
-        
-        function addExternal(obj,toolbox)
-            if obj.pStatus < obj.CONST_STATUS.loaded
+            close@toolboxClass(this)
+        end
+
+        function addExternal(this,toolbox)
+            if this.pStatus < this.CONST_STATUS.loaded
                 warning('Fieldtrip is not loaded')
                 return
             end
@@ -49,11 +51,11 @@ classdef fieldtripClass < toolboxClass
             if ~isempty(strfind(lastwarn,toolbox))
                 tbpath = strsplit(lastwarn); tbpath = tbpath{2};
             end
-            p = strsplit(path,pathsep);                
-            obj.toolInPath = vertcat(obj.toolInPath, p(cellfun(@(x) ~isempty(strfind(x,tbpath)), p)));
-            
+            p = strsplit(path,pathsep);
+            this.toolInPath = vertcat(this.toolInPath, p(cellfun(@(x) ~isempty(strfind(x,tbpath)), p)));
+
             if ~isempty(strfind(toolbox,'spm'))
-                spmVer = spm('ver'); 
+                spmVer = spm('ver');
                 global ft_default
                 ft_default.trackcallinfo = 'no';
                 ft_default.showcallinfo = 'no';
@@ -61,12 +63,12 @@ classdef fieldtripClass < toolboxClass
                 fprintf('info:SPM version %s is set\n',spmVer);
             end
         end
-        
-        function rmExternal(obj,toolbox)
-            indtbp = cellfun(@(x) ~isempty(strfind(x,toolbox)), obj.toolInPath);
-            p = obj.toolInPath(indtbp);
+
+        function rmExternal(this,toolbox)
+            indtbp = cellfun(@(x) ~isempty(strfind(x,toolbox)), this.toolInPath);
+            p = this.toolInPath(indtbp);
             rmpath(sprintf(['%s' pathsep],p{:}))
-            obj.toolInPath(indtbp) = [];
+            this.toolInPath(indtbp) = [];
         end
     end
 end
